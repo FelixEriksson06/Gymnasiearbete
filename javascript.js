dc4fe3d5acf21291e41b7fcabe5cef3c3e217782
Felix <felix.eriksson1@skola.taby.se>
2024-04-18 09:41:43 +0200

    Glömde commita förra lektionen och har även jobbat lite på andra lekioner då jag var borta för förra lektionen. jag har lagt till en sökruta, fixar så att hemsidan fungerar på mobilen och  lagt till en hamburgarsmeny.

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const url =
+  "https://cors-anywhere.herokuapp.com/https://platform.fatsecret.com/rest/server.api";
+const accessToken = "your_access_token_here";
+
+const data = {
+  method: "food.get.v2",
+  food_id: 33691,
+  format: "json",
+};
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
+function toggleMenu() {
+  var menu = document.getElementById("menu_click");
+  menu.classList.toggle("active");
+}
