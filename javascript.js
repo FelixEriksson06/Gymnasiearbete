b228806e6244d2c76c39f90362ffc6f2476946ba
Felix <felix.eriksson1@skola.taby.se>
2024-04-19 15:02:09 +0200

    Fixade en fungerande api

--- javascript.js
+++ javascript.js
@@ -1,36 +1,67 @@
-const url =
-  "https://cors-anywhere.herokuapp.com/https://platform.fatsecret.com/rest/server.api";
-const accessToken = "your_access_token_here";
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInputDesk = document.getElementById("search_text");
+  const searchInputPhone = document.getElementById("search_text_phone");
+  let foodsList = [];
 
-const data = {
-  method: "food.get.v2",
-  food_id: 33691,
-  format: "json",
-};
+  fetchFoodsList();
 
-fetch(url, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  },
-  body: JSON.stringify(data),
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+  searchInputDesk.addEventListener("input", function (event) {
+    const searchTerm = event.target.value.trim();
+    if (searchTerm !== "") {
+      searchFoods(searchTerm);
     }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
   });
+
+  searchInputPhone.addEventListener("input", function (event) {
+    const searchTerm = event.target.value.trim();
+    if (searchTerm !== "") {
+      searchFoods(searchTerm);
+    }
+  });
+
+  function fetchFoodsList() {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    const apiUrl =
+      "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel";
+
+    fetch(proxyUrl + apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (!Array.isArray(data.livsmedel)) {
+          throw new Error(
+            "Unexpected response format: livsmedel is not an array"
+          );
+        }
+        foodsList = data.livsmedel.map((food) => ({
+          id: food.nr,
+          name: food.namn,
+        }));
+      })
+      .catch((error) => {
+        console.error("Error fetching foods list:", error);
+      });
+  }
+
+  function searchFoods(searchTerm) {
+    const matchingFoods = foodsList.filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (matchingFoods.length > 0) {
+      console.log("Matching foods:", matchingFoods);
+    } else {
+      console.log("No matching foods found");
+    }
+  }
+});
 
 function toggleMenu() {
   var menu = document.getElementById("menu_click");
   menu.classList.toggle("active");
-  body.classList.toggle("active");
+  document.body.classList.toggle("active");
 }
